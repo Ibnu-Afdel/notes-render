d12880270a385536490343f51b6fa647b21446ae
Ibnu Afdel <ibnuafdel@gmail.com>
2025-02-21 20:10:56 +0300

    validation for post and put

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     response.status(400).send({ error: "malinformed id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
@@ -54,7 +56,7 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
@@ -66,9 +68,12 @@ app.post("/api/notes", (request, response) => {
     important: Boolean(body.important) || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/notes/:id", (request, response, next) => {
@@ -84,14 +89,13 @@ app.get("/api/notes/:id", (request, response, next) => {
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedNote) => {
       response.json(updatedNote);
     })
--- models/note.js
+++ models/note.js
@@ -1,34 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // const password = process.argv[2]
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-console.log('connectingto to: ', url);
-mongoose.connect(url)
-.then(result => {
-    console.log('connected to MondoDB');
-})
-.catch(error => {
-    console.log('error connecting to MongoDB: ', error.message);
-})
+console.log("connectingto to: ", url);
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MondoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB: ", error.message);
+  });
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-})
+  content: {
+    type: "String",
+    minLength: 5,
+    required: true,
+  },
+  important: Boolean,
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
-noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 // const note = new Note({
 //     content: 'personally made',
@@ -47,4 +52,4 @@ noteSchema.set('toJSON', {
 //     mongoose.connection.close()
 // })
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema);
